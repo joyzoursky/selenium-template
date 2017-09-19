b89e50b545c50b22469271b4e37ea2095256eaba
joyz <joyz@oursky.com>
2017-09-19 19:05:11 +0800

    Update to use official headless chrome

--- test_script.py
+++ test_script.py
@@ -13,6 +13,8 @@ class TestTemplate(unittest.TestCase):
         """Start web driver"""
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.implicitly_wait(10)
 
